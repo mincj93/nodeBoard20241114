8f3492cba2b23f40fea55ecb600b4ed2947eb464
mincj93 <mincj93@gmail.com>
2024-11-16 16:48:16 +0900

    ddd

--- server/routes/routers/boardRouter.js
+++ server/routes/routers/boardRouter.js
@@ -10,7 +10,8 @@ app.use(express.static(path.resolve(process.cwd() + '/public')));
 
 const boardPagePath = path.resolve(process.cwd() + '/front/board');
 router.get('/list', (요청, 응답) => {
-   응답.render('')
+   응답.render(path.join(boardPagePath + '/home'))
+   
 })
 
 module.exports = router;
