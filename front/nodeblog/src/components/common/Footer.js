46a0a5a7b442251c4c35ecc96d5a1e64491530de
kionpark <kionpark@gmail.com>
2024-11-25 17:01:14 +0900

    tw

--- front/nodeblog/src/components/common/Footer.js
+++ front/nodeblog/src/components/common/Footer.js
@@ -2,9 +2,17 @@
 // reactmoduels import
 import { useNavigate } from 'react-router-dom'
 
+
+// style
+import st from '../../style/header.module.css'
+
 // mamaterial ui
 import { Button } from '@mui/material';
-import HomeIcon from '@mui/icons-material/Home';
+import Icon_Email from '@mui/icons-material/Email';
+import Icon_GitHub from '@mui/icons-material/GitHub';
+import Icon_Create from '@mui/icons-material/Create';
+import Icon_LocalPhone from '@mui/icons-material/LocalPhone';
+
 
 function Header() {
     const navigate = useNavigate();
@@ -25,12 +33,10 @@ function Header() {
         <>
             <footer id="footer">
                 <ul class="icons">
-                    <li><a href="#" class="icon brands alt fa-twitter"><span class="label">Twitter</span></a></li>
-                    <li><a href="#" class="icon brands alt fa-facebook-f"><span class="label">Facebook</span></a></li>
-                    <li><a href="#" class="icon brands alt fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
-                    <li><a href="#" class="icon brands alt fa-instagram"><span class="label">Instagram</span></a></li>
-                    <li><a href="#" class="icon brands alt fa-github"><span class="label">GitHub</span></a></li>
-                    <li><a href="#" class="icon solid alt fa-envelope"><span class="label">Email</span></a></li>
+                    <li><a href="#" class="icon brands alt fa-instagram"><span class="label"><Icon_LocalPhone /></span></a></li>
+                    <li><a href="#" class="icon brands alt fa-instagram"><span class="label"><Icon_Create /></span></a></li>
+                    <li><a href="#" class="icon brands alt fa-github"><span class="label">GitHub<Icon_GitHub /></span></a></li>
+                    <li><a href="#" class="icon solid alt fa-envelope"><span class="label">Email<Icon_Email /></span></a></li>
                 </ul>
                 <ul class="copyright">
                     <li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
