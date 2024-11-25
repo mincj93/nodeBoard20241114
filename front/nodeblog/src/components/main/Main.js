9b537834f64f0f5e39265f699a69c097bb8090bc
kionpark <kionpark@gmail.com>
2024-11-25 16:39:00 +0900

    메인 디자인

--- front/nodeblog/src/components/main/Main.js
+++ front/nodeblog/src/components/main/Main.js
@@ -9,54 +9,69 @@ import Article from '@mui/icons-material/Article';
 // style
 import st from '../../style/main.module.css';
 
+// img
+import me from '../../images/me.jpg'
+import dog from '../../images/angryDog.jpg'
+import logo_lightsail from '../../images/logo_lightsail.png'
+import logo_express from '../../images/logo_express.png'
+import logo_mui from '../../images/logo_mui.png'
+import logo_mysql from '../../images/logo_mysql.png'
+import logo_react from '../../images/logo_react.png'
+
 const lg = console.log;
 
 const Main = () => {
   const [state, setState] = useState({
-    showBlock: false,
+    logos: [logo_react, logo_mui, logo_express, logo_mysql, logo_lightsail]
   });
 
+  const { logos } = state;
+
   return (
-    <>
+    <div style={{ minWidth: '600px' }}>
       <Header />
       <div className={st.banner_content}>
         {/* 프로필 사진 */}
         <div className={st.profile_photo}>
-          <img src={`${process.env.PUBLIC_URL}/images/me.jpg`} alt="My Profile" />
+          <img src={me} alt="My Profile" />
         </div>
         {/* 설명 */}
         <div className={st.profile_description}>
-          <h2>Hello, I'm [Your Name]</h2>
+          <p className={st.name}>민창준입니다.</p>
           <p>
-            I'm a passionate developer with expertise in React, Node.js, and modern web technologies. I enjoy solving
-            complex problems and creating delightful user experiences.
+            근데 개발을 곁들인
           </p>
-          <Button
-            variant="contained"
-            startIcon={<Article />}
-            onClick={() => lg("Learn More clicked")}
-            color="primary"
-          >
-            Learn More
-          </Button>
+
         </div>
         <div className={st.content_wrapper}>
           {/* 1/3 텍스트 영역 */}
           <div className={st.text_section}>
-            <h2>About Me</h2>
+            <h2>This web is made of</h2>
             <p>
-              I'm a passionate developer with a love for creating seamless user experiences. I specialize in React,
-              Node.js, and other modern web technologies. In my free time, I enjoy exploring new technologies and
-              contributing to open-source projects.
+              <ul>
+                <li>React</li>
+                <li>Material UI</li>
+                <li>Express Framework</li>
+                <li>MySQL</li>
+                <li>Amazone Web Service (Lightsail)</li>
+              </ul>
             </p>
+            {/* <Button
+              variant="contained"
+              startIcon={<Article />}
+              onClick={() => lg("Learn More clicked")}
+              color="primary"
+            >
+              Learn More
+            </Button> */}
           </div>
 
           {/* 3/2 이미지 영역 */}
           <div className={st.image_gallery}>
-            {[...Array(6)].map((_, idx) => (
+            {logos.map((imgSrc, idx) => (
               <div className={st.image_item} key={idx}>
                 <img
-                  src={`${process.env.PUBLIC_URL}/images/me.jpg`}
+                  src={imgSrc}
                   alt={`Sample ${idx + 1}`}
                 />
               </div>
@@ -65,7 +80,7 @@ const Main = () => {
         </div>
       </div>
       <Footer />
-    </>
+    </div>
   );
 };
 
