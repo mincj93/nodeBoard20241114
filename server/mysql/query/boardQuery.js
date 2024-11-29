3367184d22dc8a898c1313fc82c6420d7ad9cadc
kionpark <kionpark@gmail.com>
2024-11-29 13:22:46 +0900

    dd

--- server/mysql/query/boardQuery.js
+++ server/mysql/query/boardQuery.js
@@ -1,6 +1,12 @@
-const boardQuery ={};
+const boardQuery = {};
 
+// 게시물 전체
+boardQuery.getBrdDtl = `select * from board`;
+
+// 게시물 최근등록 5개
 boardQuery.getBrdLast5 = 'select * from board order by regdt desc limit 5';
+
+// 게시물 상세
 boardQuery.getBrdDtl = `select * from board where idx = ?`;
 
 module.exports = boardQuery;
